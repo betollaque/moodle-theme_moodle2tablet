149bfe2cf2a6d04deaf6a74fc9c98a32ca03ae56
betollaque <betollaque@gmail.com>
2013-06-05 15:04:51 -0500

    Fix block instance issues

--- layout/general.php
+++ layout/general.php
@@ -71,7 +71,9 @@ span.backpost a{ color: #fff; }
 <body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses.' '.join(' ', $bodyclasses)); echo ' '.$PAGE->theme->settings->choicestyle;?>">
 <?php echo $OUTPUT->standard_top_of_body_html(); 
 global $DB; 
-$instancia = $DB->get_fieldset_select('block_instances', 'id', 'blockname = \'navigation\' or blockname = \'settings\''); 
+
+$instancia_navigation = $DB->get_fieldset_select('block_instances', 'id','blockname = \'navigation\' ORDER BY id DESC');
+$instancia_settings = $DB->get_fieldset_select('block_instances', 'id','blockname = \'settings \' ORDER BY id DESC');
  
 $topsettings = $this->page->get_renderer('theme_moodle2tablet','topsettings'); // get rederer ------------------------------------------------------------------------
 ?>
@@ -114,7 +116,7 @@ $topsettings = $this->page->get_renderer('theme_moodle2tablet','topsettings'); /
 			<div class="pop-inner">
 			<div id="pages-wrapper" class="iscroller">
 					<div id="pages-iscroll">
-					<div id="inst<?php echo $instancia[0]; ?>" class="block_navigation pop-navi">
+					<div id="inst<?php echo $instancia_navigation[0]; ?>" class="block_navigation pop-navi">
                     <div class="content">
                     <?php 
 			
@@ -197,7 +199,7 @@ $topsettings = $this->page->get_renderer('theme_moodle2tablet','topsettings'); /
        <div id="classicSettings" class="tabbed pop-inner">
        <div id="pages-wrapper2" class="iscroller">
 					<div id="pages-iscroll">
-					<div id="inst<?php echo $instancia[1]; ?>" class="block_settings">
+					<div id="inst<?php echo $instancia_settings[0]; ?>" class="block_settings">
                     <div class="content">
                     <?php 
 					
